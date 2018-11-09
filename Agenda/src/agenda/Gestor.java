54facf2e1e2c246e230b89c927b0abc031861857
alumno <alumno@aula1-07>
2018-11-09 19:08:10 +0100

    mostrar personas

--- Agenda/src/agenda/Gestor.java
+++ Agenda/src/agenda/Gestor.java
@@ -23,7 +23,13 @@ public class Gestor {
     }
     
     public String mostrarTodasPersonas(){
-        String datos;
+        String datos=" ";
+        for (int i = 0; i < vAgenda.length; i++) {
+            if(vAgenda[i]!=null){
+                datos +=  vAgenda[i].toString();
+            }
+                
+        }
         
         return datos;
     }
